9542a70be056101acd4d28ea3fe461b5694f4770
Rajesh Kamireddy <rajesh17mca@gmail.com>
2024-10-16 01:55:04 -0700

    Updating app

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is / URI!'
+    return 'Hello, this is / URI and called as root!'
 
 if __name__ == '__main__':
     app.run()
